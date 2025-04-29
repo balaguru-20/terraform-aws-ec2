3d0c116d20cdefee941a5f20b3baa844090b5a03
balaguru-20 <balaguravaiah0606@gmail.com>
2025-04-29 16:11:32 +0530

    terraform module for ec2

--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 variable "ami_id" {
-    default = "ami-09c813fb71547fc4f"  
+  default = "ami-09c813fb71547fc4f"
 }
 
 variable "sg_id" { #with out value empty means mandotory
-  
+
 }
 
 variable "instance_type" {
-    default = "t3.micro"
-    validation {
-      condition = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
-      error_message = "valid values for instance type are: t3.micro t3.small t3.medium"
-    }
+  default = "t3.micro"
+  validation {
+    condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
+    error_message = "valid values for instance type are: t3.micro t3.small t3.medium"
+  }
 }
 
 variable "tags" {
-    default = {}  #it is called optional
+  default = {} #it is called optional
 }
